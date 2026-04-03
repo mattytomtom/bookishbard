35376c0baea40a166d04ed9e55443c679cfc1fa2
MattyTomTom <itmatthewt@protonmail.com>
2026-04-03 12:36:13 -0500

    Update cfg.ts

--- source/quartz/cfg.ts
+++ source/quartz/cfg.ts
@@ -50,6 +50,8 @@ export interface GlobalConfiguration {
   enableSPA: boolean
   /** Whether to display Wikipedia-style popovers when hovering over links */
   enablePopovers: boolean
+  /** Whether to enable the mobile sidebar navigation menu */
+  enableMobileSidebar: boolean
   /** Analytics mode */
   analytics: Analytics
   /** Glob patterns to not search */
